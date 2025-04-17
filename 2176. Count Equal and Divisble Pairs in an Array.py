d1056d79c709434e44cf314a8514900c03a7715a
Kyle Colby <kyleheimbigner@gmail.com>
2025-04-16 22:30:40 -0700

    Create 2176. Count Equal and Divisble Pairs in an Array.py

---
+++ 2176. Count Equal and Divisble Pairs in an Array.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def countPairs(self, nums: List[int], k: int) -> int:
+        count = 0
+        n = len(nums)
+        
+        for i in range(n):
+            for j in range(i + 1, n):  # Ensure i < j
+                if nums[i] == nums[j] and (i * j) % k == 0:
+                    count += 1
+        
+        return count
